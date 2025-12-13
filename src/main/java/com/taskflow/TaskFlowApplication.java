b8ce6666647f1d62701f50e2366113a90256feda
konrad-wojdyna <konrad.wojdyna@gmail.com>
2025-12-13 21:14:42 +0100

    feat(jpa): implement N+1 problem solutions with JOIN Fetch and @EntityGraph Performance optimization: - Indentify N+1 query problem (1 + N queries anitpattern) - Fix with JOIN FETCH in JPQL (reduces to 1 query) - Alternative solution with @EntityGraph annotation

--- src/main/java/com/taskflow/TaskFlowApplication.java
+++ src/main/java/com/taskflow/TaskFlowApplication.java
@@ -1,7 +1,13 @@
 package com.taskflow;
 
+import com.taskflow.model.Project;
+import com.taskflow.model.User;
+import com.taskflow.repository.ProjectRepository;
+import com.taskflow.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TaskFlowApplication {
@@ -9,5 +15,4 @@ public class TaskFlowApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TaskFlowApplication.class, args);
 	}
-
 }
--- src/main/java/com/taskflow/repository/ProjectRepository.java
+++ src/main/java/com/taskflow/repository/ProjectRepository.java
@@ -3,6 +3,7 @@ package com.taskflow.repository;
 
 import com.taskflow.model.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +12,9 @@ import java.util.List;
 public interface ProjectRepository extends JpaRepository<Project, Long> {
 
     List<Project> findByOwnerId(Long ownerId);
+
+
+    @Query("SELECT p FROM Project p JOIN FETCH p.owner")
+    List<Project> findAllWithOwner();
 
 }
